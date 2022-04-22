81835896dabed43a341950e17ee9d1a3149bb5fb
henrylong <henrylong719@gmail.com>
2022-04-23 07:52:10 +0930

    add bs and set up ts

---
+++ problems/704_binary_search.ts
@@ -0,0 +1,19 @@
+function search(nums: number[], target: number): number {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      return mid;
+    }
+  }
+  return -1;
+}
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9));
