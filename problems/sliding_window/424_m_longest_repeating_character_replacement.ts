eaf9ec0247e04cc166e7ff5871cccde0adc0238f
henrylong <henrylong719@gmail.com>
2022-04-26 22:35:34 +0930

    add question 424

---
+++ problems/sliding_window/424_m_longest_repeating_character_replacement.ts
@@ -0,0 +1,13 @@
+/*
+ * Input: s = "ABAB", k = 2
+ * Output: 4
+ * Explanation: Replace the two 'A's with two 'B's or vice versa.
+ *
+ *
+ */
+
+function characterReplacement(s: string, k: number): number {
+  let maxLength = 0;
+
+  return maxLength;
+}
