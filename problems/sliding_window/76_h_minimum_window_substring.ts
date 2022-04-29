72568a8be62460f24bd825b975e9c6a7dad9ed61
henrylong <henrylong719@gmail.com>
2022-04-29 21:51:09 +0930

    add problem 76

--- problems/sliding_window/76_h_minimum_window_substring.ts
+++ problems/sliding_window/76_h_minimum_window_substring.ts
@@ -1,3 +1,28 @@
 function minWindow(s: string, t: string): string {
+  // Input: String="aabdec", Pattern="abc"
+  // Output: "abdec"
+
+  // Input: String="aabdec", Pattern="abac"
+  // Output: "aabdec"
+
+  if (s.length < t.length) return '';
+
+  let windowStart = 0;
+  let frequencyMap = {} as any;
+  let letterCounter = 0;
+
+  for (let i = 0; i < t.length; i++) {
+    let char = t[i];
+
+    if (!(char in frequencyMap)) {
+      frequencyMap[char] = 0;
+    }
+    frequencyMap[char]++;
+  }
+
+  for (let windowEnd = 0; windowEnd < s.length; windowEnd++) {}
+
   return '';
 }
+
+console.log(minWindow('abdbca', 'abc'));
