da06323d6057ebd7cd6d7c8d22e01d07685ec672
henrylong <henrylong719@gmail.com>
2022-04-30 21:54:46 +0930

    add lc 219

---
+++ problems/sliding_window/219_e_contains_duplicate.ts
@@ -0,0 +1,7 @@
+// Given an integer array nums and an integer k,
+// return true if there are two distinct indices i and j in the array
+// such that nums[i] == nums[j] and abs(i - j) <= k.
+
+function containsNearbyDuplicate(nums: number[], k: number): boolean {
+  return false;
+}
