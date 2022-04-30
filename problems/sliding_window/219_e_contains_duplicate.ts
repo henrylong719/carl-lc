c2efe21c798a1a76f85805a38538bb21fa57668f
henrylong <henrylong719@gmail.com>
2022-04-30 22:11:56 +0930

    add lc 219 solution

--- problems/sliding_window/219_e_contains_duplicate.ts
+++ problems/sliding_window/219_e_contains_duplicate.ts
@@ -3,5 +3,25 @@
 // such that nums[i] == nums[j] and abs(i - j) <= k.
 
 function containsNearbyDuplicate(nums: number[], k: number): boolean {
+  let charMap = {} as any;
+
+  // loop nums
+  for (let i = 0; i < nums.length; i++) {
+    let char = nums[i];
+
+    // check if the charcter already in the hash map
+    if (!(char in charMap)) {
+      // not, add it and its postion
+      charMap[char] = i;
+    } else {
+      // ums[i] == nums[j] and abs(i - j) <= k. reurn true
+      if (i - charMap[char] <= k) {
+        return true;
+      } else {
+        // record current posion
+        charMap[char] = i;
+      }
+    }
+  }
   return false;
 }
