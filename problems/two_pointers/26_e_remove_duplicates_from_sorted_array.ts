d11dc619d6434c146cb1a809fe9273f66c3d6bef
henrylong <henrylong719@gmail.com>
2022-05-04 21:13:24 +0930

    add lc 977

--- problems/two_pointers/26_e_remove_duplicates_from_sorted_array.ts
+++ problems/two_pointers/26_e_remove_duplicates_from_sorted_array.ts
@@ -1,3 +1,18 @@
 function removeDuplicates(nums: number[]): number {
-  return -1;
+  let nextDuplicate = 1;
+
+  let i = 0;
+
+  while (i < nums.length) {
+    if (nums[nextDuplicate - 1] !== nums[i]) {
+      nums[nextDuplicate] = nums[i];
+      nextDuplicate++;
+    }
+
+    i++;
+  }
+
+  return nextDuplicate;
 }
+
+console.log(removeDuplicates([2, 2, 2, 11]));
