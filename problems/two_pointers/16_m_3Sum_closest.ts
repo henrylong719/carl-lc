94b25cdf25b196c44f8dda431e9cea8914892636
henrylong <henrylong719@gmail.com>
2022-05-08 18:25:30 +0930

    add soution for lc 16

---
+++ problems/two_pointers/16_m_3Sum_closest.ts
@@ -0,0 +1,36 @@
+function threeSumClosest(nums: number[], target: number): number {
+  nums.sort((a, b) => a - b);
+
+  let smallest_difference = Infinity;
+
+  for (let i = 0; i < nums.length; i++) {
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      const target_diff = target - nums[i] - nums[left] - nums[right];
+
+      if (target_diff === 0) {
+        return target;
+      }
+
+      if (
+        Math.abs(target_diff) < Math.abs(smallest_difference) ||
+        (Math.abs(target_diff) === Math.abs(smallest_difference) &&
+          target_diff > smallest_difference)
+      ) {
+        smallest_difference = target_diff;
+      }
+
+      if (target_diff > 0) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return smallest_difference;
+}
+
+console.log(threeSumClosest([-2, 0, 1, 2], 2));
