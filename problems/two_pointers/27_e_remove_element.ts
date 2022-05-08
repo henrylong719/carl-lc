e1ddb619451e8428e72ef61ba1c5de1abc6f5781
henrylong <henrylong719@gmail.com>
2022-05-08 15:00:40 +0930

    add lc 27

---
+++ problems/two_pointers/27_e_remove_element.ts
@@ -0,0 +1,12 @@
+function removeElement(nums: number[], val: number): number {
+  let nextElement = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[nextElement] = nums[i];
+      nextElement++;
+    }
+  }
+
+  return nextElement;
+}
