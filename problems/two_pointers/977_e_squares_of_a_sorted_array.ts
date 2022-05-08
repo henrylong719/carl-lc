2ba2960677309b0196d7e6f5a7ceb7ef62433adf
henrylong <henrylong719@gmail.com>
2022-05-08 15:25:32 +0930

    add problem 15

--- problems/two_pointers/977_e_squares_of_a_sorted_array.ts
+++ problems/two_pointers/977_e_squares_of_a_sorted_array.ts
@@ -1,5 +1,28 @@
+// Input: [-2, -1, 0, 2, 3]
+// Output: [0, 1, 4, 4, 9]
+
 function sortedSquares(nums: number[]): number[] {
-  return [];
+  let resultArr = Array(nums.length).fill(0);
+  let leftPointer = 0;
+  let rightPointer = nums.length - 1;
+  let value = nums.length - 1;
+
+  while (leftPointer < rightPointer) {
+    const leftValue = nums[leftPointer] * nums[leftPointer];
+    const rightValue = nums[rightPointer] * nums[rightPointer];
+
+    if (leftValue > rightValue) {
+      resultArr[value] = leftValue;
+      leftPointer++;
+    } else {
+      resultArr[value] = rightValue;
+      rightPointer--;
+    }
+
+    value--;
+  }
+
+  return resultArr;
 }
 
-console.log(Array(7).fill(0));
+console.log(sortedSquares([-2, -1, 0, 2, 3]));
