02bf3ab451616f2d03f87b77f0412852dcc7e271
henrylong <henrylong719@gmail.com>
2022-05-09 22:16:33 +0930

    add lc 75

--- problems/two_pointers/713_m_subarray_product_less_than_K.ts
+++ problems/two_pointers/713_m_subarray_product_less_than_K.ts
@@ -1,3 +1,32 @@
-function numSubarrayProductLessThanK(nums: number[], k: number): number {
-  return 0;
+// [2, 5, 3, 10], target=30
+
+function numSubarrayProductLessThanK(nums: number[], k: number): number[] {
+  let resultArr = [] as any[];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < k) {
+      resultArr.push([nums[i]]);
+      let currentProduct = nums[i];
+      let tmpArr = [];
+
+      tmpArr.push(nums[i]);
+      let signal = false;
+      for (let j = i + 1; j < nums.length; j++) {
+        currentProduct = currentProduct * nums[j];
+
+        console.log(currentProduct);
+        if (currentProduct < k) {
+          tmpArr.push(nums[j]);
+          signal = true;
+        } else {
+          break;
+        }
+      }
+      signal && resultArr.push(tmpArr);
+    }
+  }
+
+  return resultArr;
 }
+
+console.log(numSubarrayProductLessThanK([10, 5, 2, 6], 100));
---
+++ problems/two_pointers/75_m_sort_colors.ts
@@ -0,0 +1,4 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function sortColors(nums: number[]): void {}
