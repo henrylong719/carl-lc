c04e38f50948f701d2e8ef1cec1b58441912ef61
henrylong <henrylong719@gmail.com>
2022-05-09 21:25:00 +0930

    add triplet with smaller sum II

--- problems/two_pointers/triplet_with_smaller_sum.ts
+++ problems/two_pointers/triplet_with_smaller_sum.ts
@@ -48,4 +48,45 @@ function searchPair(arr: number[], targetSum: number, first: number): number {
   return count;
 }
 
-console.log(triplet_with_smaller_sum([-1, 4, 2, 1, 3], 5));
+// console.log(triplet_with_smaller_sum([-1, 4, 2, 1, 3], 5));
+
+function triplet_with_smaller_sum_II(arr: number[], target: number) {
+  arr.sort((a, b) => a - b);
+  let triplets: number[][] = [];
+
+  for (let i = 0; i < arr.length - 2; i++) {
+    if (i > 0 && arr[i] === arr[i - 1]) continue;
+    // i + left + right < target
+    // left + right < target - i
+    // searchPair_II()
+    searchPair_II(arr, target - arr[i], i, triplets);
+  }
+
+  return triplets;
+}
+
+function searchPair_II(
+  arr: number[],
+  target_sum: number,
+  first: number,
+  triplets: number[][]
+) {
+  let left = first + 1;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let currentSum = arr[left] + arr[right];
+
+    if (currentSum < target_sum) {
+      for (let i = right; i > left; i--) {
+        triplets.push([arr[first], arr[left], arr[i]]);
+      }
+
+      left++;
+    } else {
+      right--;
+    }
+  }
+}
+
+console.log(triplet_with_smaller_sum_II([-1, 4, 2, 1, 3], 5));
