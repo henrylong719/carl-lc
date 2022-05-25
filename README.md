bba000de88fef4b74cbace476bad2fcb3a0d505a
henrylong <henrylong719@gmail.com>
2022-05-25 22:32:41 +0930

    add fast & slow pointers pattern

--- README.md
+++ README.md
@@ -212,6 +212,18 @@ const pair_with_targetsum = function(arr, target_sum) {
 
 
 
+## Fast & Slow Pointers
+
+The **Fast & Slow** pointer approach, also known as the **Hare & Tortoise algorithm**, is a pointer algorithm that uses two pointers which move through the array (or sequence/LinkedList) at different speeds. This approach is quite useful when dealing with cyclic LinkedLists or arrays.
+
+By moving at different speeds (say, in a cyclic LinkedList), the algorithm proves that the two pointers are bound to meet. The fast pointer should catch the slow pointer once both the pointers are in a cyclic loop.
+
+
+
+
+
+
+
 
 
 
--- problems/two_pointers/581_m_shortest_unsorted_continuous_subarray.ts
+++ problems/two_pointers/581_m_shortest_unsorted_continuous_subarray.ts
@@ -1,3 +1,41 @@
-function findUnsortedSubarray(nums: number[]): number {
-  return 0;
+function shortest_window_sort(arr: number[]): number {
+  let low = 0;
+  let high = arr.length - 1;
+
+  // find the first index out of order from left
+  while (low < arr.length && arr[low] <= arr[low + 1]) {
+    low++;
+  }
+
+  // all numbers are in ascending order
+  if (low === arr.length - 1) return 0;
+
+  // find the first index out of order from right
+  while (high > 0 && arr[high - 1] <= arr[high]) {
+    high--;
+  }
+
+  // find the min and max number in the subarray between low and high
+
+  let maxNumInSubArr = -Infinity;
+  let minNumInSubArr = Infinity;
+
+  for (let k = low; k <= high; k++) {
+    maxNumInSubArr = Math.max(maxNumInSubArr, arr[k]);
+    minNumInSubArr = Math.min(minNumInSubArr, arr[k]);
+  }
+
+  // extend the low from left
+  while (low > 0 && arr[low - 1] > minNumInSubArr) {
+    low--;
+  }
+
+  // extend the high from right
+  while (high < arr.length - 1 && arr[high + 1] < maxNumInSubArr) {
+    high++;
+  }
+
+  return high - low + 1;
 }
+
+console.log(shortest_window_sort([1, 2, 3, 3, 3]));
