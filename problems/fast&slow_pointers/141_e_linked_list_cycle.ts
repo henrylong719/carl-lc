87b53b42d15fd4b45e0075c7ab24c7429018c636
henrylong <henrylong719@gmail.com>
2022-07-10 13:42:31 +0930

    add comments

--- problems/fast&slow_pointers/141_e_linked_list_cycle.ts
+++ problems/fast&slow_pointers/141_e_linked_list_cycle.ts
@@ -7,16 +7,20 @@
 //   }
 // }
 
-function hasCycle(head: ListNode | null): boolean {
+function has_cycle(head: ListNode | null) {
   let slow = head;
   let fast = head;
 
   while (fast !== null && fast.next !== null) {
+    // fast pointer is one step before slow pointer
     fast = fast.next.next;
     slow = slow!.next;
 
-    if (fast === slow) return true;
+    // two pointer in the same position
+    while (slow === fast) {
+      // found the cycle
+      return true;
+    }
   }
-
   return false;
 }
