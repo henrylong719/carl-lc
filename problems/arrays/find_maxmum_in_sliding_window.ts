e5ed5fa369e81e64b7a1ba031c5d02e12fa765c8
henrylong719 <henrylong719@gmail.com>
2022-11-25 20:37:55 +1030

    add array problems

---
+++ problems/arrays/find_maxmum_in_sliding_window.ts
@@ -0,0 +1,12 @@
+/**
+ * Given an integer array and a window of size w, find the current maximum value in the window
+ * as it slides through the entire array.
+ *
+ * Sample Input:
+ * nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ * window_size = 3
+ *
+ * Expected Output:
+ * [3, 4, 5, 6, 7, 8, 9, 10]
+ *
+ */
