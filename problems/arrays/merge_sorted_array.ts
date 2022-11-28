d87d8e82e6b182b55ff297ce9144c82d33405bf8
henrylong719 <henrylong719@gmail.com>
2022-11-29 08:19:46 +1030

    add merge arrays method two

--- problems/arrays/merge_sorted_array.ts
+++ problems/arrays/merge_sorted_array.ts
@@ -1,3 +1,36 @@
+function mergeArraysTwo(arr1: number[], arr2: number[]) {
+  let merge = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      merge.push(arr1[i]);
+      i++;
+    } else {
+      merge.push(arr2[j]);
+      j++;
+    }
+  }
+
+  if (i <= arr1.length - 1) {
+    arr1.splice(0, i);
+    merge = merge.concat(arr1);
+  } else if (j <= arr2.length - 1) {
+    arr2.splice(0, j);
+    merge = merge.concat(arr2);
+  }
+
+  return merge;
+}
+
+let arr1 = [1, 3, 4, 5];
+let arr2 = [2, 6, 7, 8];
+
+console.log(mergeArraysTwo(arr1, arr2));
+
+export {};
+
 function mergeArrays(arr1: number[], arr2: number[]) {
   const combinedArr = [...arr1, ...arr2];
 
@@ -14,10 +47,3 @@ function mergeArrays(arr1: number[], arr2: number[]) {
 
   return combinedArr;
 }
-
-let arr1 = [1, 3, 4, 5];
-let arr2 = [2, 6, 7, 8];
-
-console.log(mergeArrays(arr1, arr2));
-
-export {};
