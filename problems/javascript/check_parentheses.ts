837ead9db1d11e9f33b1a5d32339c7ff0deccbba
henrylong719 <henrylong719@gmail.com>
2022-11-29 20:41:20 +1030

    add check parentheses solution two using map

--- problems/javascript/check_parentheses.ts
+++ problems/javascript/check_parentheses.ts
@@ -29,10 +29,36 @@ function balancedParentheses(str: string) {
   return strArr.length === 0;
 }
 
+// time complexity O(n)
+function balancedParenthesesTwo(str: string) {
+  let stack = [];
+  let map = {
+    '{': '}',
+    '[': ']',
+    '(': ')',
+  };
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === ' ') continue;
+
+    if (str[i] === '{' || str[i] === '[' || str[i] === '(') {
+      stack.push(str[i]);
+      continue;
+    }
+
+    const lastEle = stack.pop() as string;
+
+    if (str[i] !== map[lastEle as keyof typeof map]) return false;
+  }
+
+  if (stack.length !== 0) return false;
+  return true;
+}
+
 // true
-console.log(balancedParentheses('{ [ ] ( ) }'));
+console.log(balancedParenthesesTwo('{ [ ] ( ) }'));
 
 // false
-console.log(balancedParentheses('{ [ ( ] ) }'));
+console.log(balancedParenthesesTwo('{ [ ( ] ) }'));
 
 export {};
