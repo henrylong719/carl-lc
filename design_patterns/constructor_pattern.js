8627d1ae73a22434351186e2c7c0c5101bf6bafb
henrylong719 <henrylong719@gmail.com>
2022-12-04 16:50:13 +1030

    add constructor pattern

---
+++ design_patterns/constructor_pattern.js
@@ -0,0 +1,13 @@
+// function Shape(color, sides, name){
+//   this.color = color;
+//   this.sides = sides;
+//   this.name = name;
+// }
+
+class Shape {
+  constructor(color, sides, name) {
+    this.color = color;
+    this.sides = sides;
+    this.name = name;
+  }
+}
