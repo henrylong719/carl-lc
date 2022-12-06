b7ded78f3a777a2288b6e570da14684bb0fb0d95
henrylong719 <henrylong719@gmail.com>
2022-12-06 21:08:49 +1030

    add solution 2 for find array product

--- problems/arrays/find_array_product.js
+++ problems/arrays/find_array_product.js
@@ -1,9 +1,33 @@
+// complexity: O(n)
 function findProduct(arr) {
   let productArr = [];
 
   const arrProduct = arr.reduce((acc, cur) => acc * cur);
-
   arr.forEach((ele, index) => (productArr[index] = arrProduct / ele));
 
   return productArr;
 }
+
+// complexity O(n^2)
+
+function findProductTwo(arr) {
+  let result = [];
+  let currentProduct;
+  let leftSideProduct = 1;
+
+  for (let i = 0; i < arr.length; i++) {
+    currentProduct = 1;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      currentProduct *= arr[j];
+    }
+
+    result.push(leftSideProduct * currentProduct);
+
+    leftSideProduct *= arr[i];
+  }
+
+  return result;
+}
+
+console.log(findProduct([2, 5, 9, 3, 6]));
