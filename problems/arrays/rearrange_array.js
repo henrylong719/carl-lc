2db9319067c04d3b9c3fb3c240667ce00fb071c3
henrylong719 <henrylong719@gmail.com>
2022-12-07 13:34:24 +1030

    add another solution for rearrange array

--- problems/arrays/rearrange_array.js
+++ problems/arrays/rearrange_array.js
@@ -13,4 +13,22 @@ function reArrange(arr) {
   return [...negativeArr, ...positiveArr];
 }
 
-console.log(reArrange([10, -1, 20, 4, 5, -9, -6]));
+function reArrangeTwo(arr) {
+  let leftMostPosEle = 0;
+  let tmp;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+      if (i !== leftMostPosEle) {
+        // change position
+        tmp = arr[leftMostPosEle];
+        arr[leftMostPosEle] = arr[i];
+        arr[i] = tmp;
+      }
+      leftMostPosEle++;
+    }
+  }
+
+  return arr;
+}
+
+console.log(reArrangeTwo([10, -1, 20, 4, 5, -9, -6]));
