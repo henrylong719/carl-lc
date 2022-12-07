44d623d2b846784d17cdbe9effa4a5444f11f91d
henrylong719 <henrylong719@gmail.com>
2022-12-07 11:40:09 +1030

    add right rotate array

---
+++ problems/arrays/right_rotate_array.js
@@ -0,0 +1,27 @@
+function rightRotate(arr, n) {
+  for (let i = 0; i < n; i++) {
+    const ele = arr.pop();
+    arr.unshift(ele);
+  }
+
+  return arr;
+}
+
+// manual rotation
+function rightRotateTwo(arr, n) {
+  const result = [];
+
+  for (let i = arr.length - n; i < arr.length; i++) {
+    result.push(arr[i]);
+  }
+
+  for (let j = 0; j < arr.length - n; j++) {
+    result.push(arr[j]);
+  }
+
+  return result;
+}
+
+const arr = [1, 2, 3, 4, 5];
+
+console.log(rightRotateTwo(arr, 2));
