6032c58623d791199c5f16120e124e48bb7027f2
henrylong719 <henrylong719@gmail.com>
2022-12-09 09:17:30 +1030

    add deletion by value in linked list solution 2

--- problems/linked_list/deletion_by_value.js
+++ problems/linked_list/deletion_by_value.js
@@ -2,24 +2,50 @@
 const LinkedList = require('./LinkedList.js');
 const Node = require('./Node.js');
 
-LinkedList.prototype.deleteVal = function (value) {
-  let deleted = false;
-  let currentNode = this.head;
-  let previousNode = null;
+// LinkedList.prototype.deleteVal = function (value) {
+//   let deleted = false;
+//   let currentNode = this.head;
+//   let previousNode = null;
 
-  while (currentNode !== null) {
-    if (currentNode.data === value) {
-      if (previousNode !== null) {
-        previousNode.nextElement = currentNode.nextElement;
-        deleted = true;
-        return deleted;
-      }
-      this.head = currentNode.nextElement;
+//   while (currentNode !== null) {
+//     if (currentNode.data === value) {
+//       if (previousNode !== null) {
+//         previousNode.nextElement = currentNode.nextElement;
+//         deleted = true;
+//         return deleted;
+//       }
+//       this.head = currentNode.nextElement;
+//     }
+//     previousNode = currentNode;
+//     currentNode = currentNode.nextElement;
+//   }
+
+//   //Write code here
+//   return deleted;
+// };
+
+// LinkedList = 3->2->1->0
+// integer = 1
+// solution 2
+LinkedList.prototype.deleteVal = function (value) {
+  let currentNode = this.head;
+
+  if (this.isEmpty()) {
+    return false;
+  }
+
+  if (currentNode.data === value) {
+    this.head = currentNode.nextElement;
+    return true;
+  }
+
+  while (currentNode.nextElement !== null) {
+    if (currentNode.nextElement.data === value) {
+      currentNode.nextElement = currentNode.nextElement.nextElement;
+      return true;
     }
-    previousNode = currentNode;
     currentNode = currentNode.nextElement;
   }
 
-  //Write code here
-  return deleted;
+  return false;
 };
