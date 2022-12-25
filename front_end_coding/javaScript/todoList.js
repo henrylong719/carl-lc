a50af54093e24403537c88ecee9cf396ed28551c
henrylong719 <henrylong719@gmail.com>
2022-12-25 16:56:26 +1030

    use IIFE for todo

--- front_end_coding/javaScript/todoList.js
+++ front_end_coding/javaScript/todoList.js
@@ -1,37 +1,39 @@
 import './styles.css';
 
-const inputEle = document.querySelector('#todoInput');
-const ul = document.querySelector('#todos');
+(() => {
+  const inputEle = document.querySelector('#todoInput');
+  const ul = document.querySelector('#todos');
 
-// add todos
-function addTodos(label) {
-  const li = document.createElement('li');
-  const span = document.createElement('span');
-  const deleteBtn = document.createElement('button');
-  span.innerText = label;
-  deleteBtn.innerText = 'Delete';
-  li.appendChild(span);
-  li.appendChild(deleteBtn);
-  ul.appendChild(li);
-}
-
-// remove todos
-function removeTodos(ele) {
-  ele.parentNode.removeChild(ele);
-}
-
-document.querySelector('#submitBtn').addEventListener('click', function () {
-  const inputValue = inputEle.value;
-  if (!inputValue) {
-    alert('please input a value !');
-    return;
+  // add todos
+  function addTodos(label) {
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    const deleteBtn = document.createElement('button');
+    span.innerText = label;
+    deleteBtn.innerText = 'Delete';
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    ul.appendChild(li);
   }
-  addTodos(inputValue);
-  inputEle.value = '';
-});
 
-ul.addEventListener('click', function (event) {
-  if (event.target.tagName === 'BUTTON') {
-    removeTodos(event.target.parentNode);
+  // remove todos
+  function removeTodos(ele) {
+    ele.parentNode.removeChild(ele);
   }
-});
+
+  document.querySelector('#submitBtn').addEventListener('click', function () {
+    const inputValue = inputEle.value;
+    if (!inputValue) {
+      alert('please input a value !');
+      return;
+    }
+    addTodos(inputValue);
+    inputEle.value = '';
+  });
+
+  ul.addEventListener('click', function (event) {
+    if (event.target.tagName === 'BUTTON') {
+      removeTodos(event.target.parentNode);
+    }
+  });
+})();
