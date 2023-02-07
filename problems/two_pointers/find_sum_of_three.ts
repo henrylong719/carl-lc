cd4795b8a8357b166d41624c98969d13b9a8e295
henrylong719 <henrylong719@gmail.com>
2023-02-07 22:56:05 +1030

    add unfinished sum of three

--- problems/two_pointers/find_sum_of_three.ts
+++ problems/two_pointers/find_sum_of_three.ts
@@ -1,3 +1,12 @@
 export function findSumOfThree(nums: number[], target: number) {
+  // a + b + c = target
+  // a + b = target - c
+
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+  }
+
   return false;
 }
