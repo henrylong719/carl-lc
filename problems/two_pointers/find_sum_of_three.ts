a2521556ff3f8a23bc14707942c2e3eca5b248d0
henrylong719 <henrylong719@gmail.com>
2023-02-08 20:39:14 +1030

    add solution for find sum of three

--- problems/two_pointers/find_sum_of_three.ts
+++ problems/two_pointers/find_sum_of_three.ts
@@ -1,12 +1,30 @@
 export function findSumOfThree(nums: number[], target: number) {
-  // a + b + c = target
-  // a + b = target - c
-
   nums.sort((a, b) => a - b);
 
   for (let i = 0; i < nums.length; i++) {
     if (i > 0 && nums[i] === nums[i - 1]) continue;
+    const targetValue = target - nums[i];
+
+    if (findSum(nums, i + 1, targetValue)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function findSum(nums: number[], left: number, targetValue: number) {
+  let right = nums.length - 1;
+
+  while (nums[left] < nums[right]) {
+    if (nums[left] + nums[right] < targetValue) {
+      left++;
+    } else if (nums[left] + nums[right] > targetValue) {
+      right--;
+    } else {
+      return true;
+    }
   }
 
   return false;
 }
+console.log(findSumOfThree([1, -1, 0], -1));
