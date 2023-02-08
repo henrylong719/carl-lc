da39895c304489fc64b274df60fd64247cd545f0
henrylong719 <henrylong719@gmail.com>
2023-02-09 08:56:06 +1030

    add reverse words

---
+++ problems/two_pointers/reverse_words.ts
@@ -0,0 +1,28 @@
+export function reverseWords(sentence: string) {
+  return sentence
+    .split(' ')
+    .filter((word) => !!word)
+    .reverse();
+}
+
+// reverse words using double pointer
+
+export function reverseWordsUsingDoublePointer(sentence: string) {
+  const wordsArray = sentence.split(' ').filter((w) => !!w);
+
+  let left = 0;
+  let right = wordsArray.length - 1;
+
+  while (left < right) {
+    let temp = wordsArray[right];
+    wordsArray[right] = wordsArray[left];
+    wordsArray[left] = temp;
+
+    left++;
+    right--;
+  }
+
+  return wordsArray.join(' ');
+}
+
+console.log(reverseWordsUsingDoublePointer('Hello     World'));
