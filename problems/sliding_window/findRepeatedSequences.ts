5c88a084b610a04a5f8536eaa8a9f418442d8b9f
henrylong719 <henrylong719@gmail.com>
2023-02-13 21:32:19 +1030

    add best solution for find repeated sequences

---
+++ problems/sliding_window/findRepeatedSequences.ts
@@ -0,0 +1,32 @@
+/**
+ *
+ *
+ * "AAAAACCCCCAAAAACCCCCC" , 8
+ * "GGGGGGGGGGGGGGGGGGGGGGGGG" , 12
+ * "TTTTTCCCCCCCTTTTTTCCCCCCCTTTTTTT" , 10
+ * "AAAAAACCCCCCCAAAAAAAACCCCCCCTG" , 10
+ * "ATATATATATATATAT" , 6
+ *
+ *
+ */
+
+export function findRepeatedSequences(s: string, k: number) {
+  if (s.length < k) return [];
+
+  let seen = new Map();
+  let result = new Map();
+
+  for (let i = 0; i < s.length; i++) {
+    let cur = s.slice(i, i + k);
+
+    if (seen.has(cur)) {
+      result.set(cur, 1);
+    }
+
+    seen.set(cur, 1);
+  }
+
+  return Array.from(result.keys());
+}
+
+console.log(findRepeatedSequences('AAAAACCCCCAAAAACCCCCC', 8));
---
+++ problems/sliding_window/minWindow.ts
@@ -0,0 +1,17 @@
+/***
+ *
+ * "abcdebdde" , "bde"
+ * "fgrqsqsnodwmxzkzxwqegkndaa" , "kzed"
+ * "michmznaitnjdnjkdsnmichmznait" , "michmznait"
+ * "afgegrwgwga" , "aa"
+ * "abababa" , "ba"
+ *
+ */
+
+export function minWindow(str1: string, str2: string) {
+  let window = '';
+
+  for (let i = 0; i < str2.length; i++) {}
+
+  return '';
+}
