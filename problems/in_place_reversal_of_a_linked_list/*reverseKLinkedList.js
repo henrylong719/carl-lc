efb87b11f01cb849947cd60a8537621826f08a16
henrylong719 <henrylong719@gmail.com>
2023-02-23 22:15:14 +1030

    add reverse k linked list

---
+++ problems/in_place_reversal_of_a_linked_list/*reverseKLinkedList.js
@@ -0,0 +1,18 @@
+import LinkedList from './linked_list.js';
+import LinkedListNode from './linked_list_node.js';
+import traverseLinkedList from './linked_list_traversal.js';
+import reverseList from './linked_list_reversal.js';
+
+/*
+ * [1,2,3,4,5] 2
+ *
+ * [3,4,5,6,2,8,7,7] 3
+ *
+ * [1,2,3,4,5] 1
+ *
+ * [1,2,3,4,5,6,7] 3
+ *
+ * [1,2,3,4,5,6,7] 7
+ */
+
+export function reverseKLinkedList(head, k) {}
