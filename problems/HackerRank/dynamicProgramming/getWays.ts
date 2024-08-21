ba03ef971f9b85ae34f569077fee753f0d9a0519
henrylong <henrylong719@gmail.com>
2024-08-21 18:19:07 +1000

    add getWays

--- problems/HackerRank/dynamicProgramming/getWays.ts
+++ problems/HackerRank/dynamicProgramming/getWays.ts
@@ -1,15 +1,24 @@
-function getWays(n: number, c: number[]): number {
-  let result = 0;
+// function getWays(n: number, c: number[]): number {
+//   // Initialize an array called 'ways' of length 'n + 1' and fill it with 0s.
+//   let ways = new Array(n + 1).fill(0);
+//   // Set the number of ways to make change for the amount 0 to 1.
+//   ways[0] = 1;
 
-  for (let ele of c) {
-    if (ele > n) {
-      continue;
-    }
+//   // Iterate through each coin denomination in the array 'c'.
+//   // c = [8,3,1,2]
+//   // n = 3
+//   for (let coin of c) {
+//     // For each coin denomination, iterate through each amount from 'coin' to 'n'.
+//     for (let amount = coin; amount <= n; amount++) {
+//       // Update the number of ways to make change for the current 'amount'
+//       // by adding the number of ways to make change for 'amount - coin'
+//       // (i.e., the current amount minus the current coin denomination).
+//       ways[amount] += ways[amount - coin];
+//     }
+//   }
 
-    if (ele === n) {
-      result++;
-    }
-  }
+//   // Return the number of ways to make change for the target amount 'n'.
+//   return ways[n];
+// }
 
-  return result;
-}
+// getWays(3, [8, 3, 1, 2]);
