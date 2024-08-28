3e2e25d537887cac4a80c184d38b6e993e83d6c6
henrylong <henrylong719@gmail.com>
2024-08-28 17:14:31 +1000

    add dynamicArray

---
+++ problems/HackerRank/dynamicArray.ts
@@ -0,0 +1,19 @@
+function dynamicArray(n: number, queries: number[][]): number[] {
+  const arr = Array.from({ length: n }, () => []) as any;
+  const ans = [];
+  let lastAnswer = 0;
+
+  for (let i = 0; i < queries.length; i++) {
+    let x = Number(queries[i][1]);
+    let y = Number(queries[i][2]);
+    let idx = (x ^ lastAnswer) % n;
+
+    if (Number(queries[i][0]) === 1) {
+      arr[idx].push(y);
+    } else if (Number(queries[i][0]) === 2) {
+      lastAnswer = arr[idx][y % arr[idx].length];
+      ans.push(lastAnswer);
+    }
+  }
+  return ans;
+}
