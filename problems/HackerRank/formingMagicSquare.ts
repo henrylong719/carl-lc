a720e5acbae782ff6bd1e1960c2099fd6202902a
henrylong <henrylong719@gmail.com>
2024-09-07 15:17:17 +1000

    add formingMagicSquare solution

--- problems/HackerRank/formingMagicSquare.ts
+++ problems/HackerRank/formingMagicSquare.ts
@@ -1,3 +1,67 @@
 function formingMagicSquare(s: number[][]): number {
-  // Write your code here
+  const patterns = [
+    [
+      [8, 1, 6],
+      [3, 5, 7],
+      [4, 9, 2],
+    ],
+    [
+      [6, 1, 8],
+      [7, 5, 3],
+      [2, 9, 4],
+    ],
+    [
+      [4, 9, 2],
+      [3, 5, 7],
+      [8, 1, 6],
+    ],
+    [
+      [2, 9, 4],
+      [7, 5, 3],
+      [6, 1, 8],
+    ],
+    [
+      [8, 3, 4],
+      [1, 5, 9],
+      [6, 7, 2],
+    ],
+    [
+      [4, 3, 8],
+      [9, 5, 1],
+      [2, 7, 6],
+    ],
+    [
+      [6, 7, 2],
+      [1, 5, 9],
+      [8, 3, 4],
+    ],
+    [
+      [2, 7, 6],
+      [9, 5, 1],
+      [4, 3, 8],
+    ],
+  ];
+
+  let min = Infinity;
+
+  for (let ele of patterns) {
+    let temp = 0;
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        temp += Math.abs(ele[i][j] - s[i][j]);
+      }
+    }
+    min = Math.min(min, temp);
+  }
+
+  return min;
 }
+
+console.log(
+  formingMagicSquare([
+    [5, 3, 4],
+    [1, 5, 8],
+    [6, 4, 2],
+  ])
+);
