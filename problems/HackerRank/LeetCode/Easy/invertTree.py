19e4d378ddbcb31cffa66a7e5604eb2871a55f73
henrylong <henrylong719@gmail.com>
2024-12-16 11:12:53 +1100

    add invertTree solution

---
+++ problems/HackerRank/LeetCode/Easy/invertTree.py
@@ -0,0 +1,11 @@
+   def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root == None:
+            return None
+
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
