f17cd8e2124f725468205c40064c51563ca26155
henrylong <henrylong719@gmail.com>
2024-12-19 20:33:50 +1100

    add leetcode isSameTree

---
+++ problems/HackerRank/2521_exercises/tree/isSameTree.c
@@ -0,0 +1,15 @@
+bool isSameTree(struct TreeNode *p, struct TreeNode *q)
+{
+
+	if (p == NULL && q == NULL)
+	{
+		return true;
+	}
+
+	if ((p == NULL || q == NULL) || (p->val != q->val))
+	{
+		return false;
+	}
+
+	return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
