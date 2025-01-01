2ca613cfb2f2e0d15d29b28b58b4bb142d57590e
henrylong <henrylong719@gmail.com>
2025-01-01 17:27:08 +1100

    add leetcode Subarray Product Less Than K

---
+++ problems/LeetCode/sliding_window/numSubarrayProductLessThanK.py
@@ -0,0 +1,18 @@
+def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+		if k <= 1:
+				return 0
+
+		prod = 1
+		ans = 0
+		left = 0
+
+		for right, x in enumerate(nums):
+				prod *= x
+
+				while prod >= k:
+						prod /= nums[left]
+						left += 1
+				
+				ans += right - left + 1
+		
+		return ans
