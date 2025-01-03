18e38300da057fc8e6b84aa7261e8cc4eec64a90
henrylong <henrylong719@gmail.com>
2025-01-03 17:10:33 +1100

    add leetcode Successful Pairs of Spells and Potions

---
+++ problems/LeetCode/binary_search/maximumCount.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+def lower_bound(nums: List[int], target: int) -> int:
+		left, right = 0, len(nums) - 1
+
+		while left <= right:
+
+				mid = (left + right) // 2
+
+				if target <= nums[mid]:
+						right = mid - 1
+				else:
+						left = mid + 1
+		
+		return left
+
+
+print(lower_bound([-3,-2,-1,0,0,0],1))
