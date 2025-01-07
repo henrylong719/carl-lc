2b7f8ff17a5c8ea270157dbd6f1955065ffb5697
henrylong <henrylong719@gmail.com>
2025-01-07 20:32:17 +1100

    add leetcode Search in Rotated Sorted Array

--- problems/LeetCode/binary_search/findMin.py
+++ problems/LeetCode/binary_search/findMin.py
@@ -1,2 +1,20 @@
 def findMin(self, nums: List[int]) -> int:
-  
+		ans = nums[0]
+		left, right = 0, len(nums) - 1
+
+		while left <= right:
+				# already in sorted array
+				if nums[left] < nums[right]:
+						ans = min(ans, nums[left])
+						break
+				
+				mid = (left + right) // 2
+				ans = min(ans, nums[mid])
+
+				# in the left part
+				if nums[mid] >= nums[left]:
+						left = mid + 1
+				else:
+						right = mid - 1
+		
+		return ans
