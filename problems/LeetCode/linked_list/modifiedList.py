38157f43f1721d637b07373e88026b71b55687e7
henrylong <henrylong719@gmail.com>
2025-01-15 17:00:32 +1100

    add leetcode Delete Nodes From Linked List Present in Array

--- problems/LeetCode/linked_list/modifiedList.py
+++ problems/LeetCode/linked_list/modifiedList.py
@@ -1,2 +1,13 @@
 def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
-  
+		dummyNode = ListNode(next = head)
+		cur = dummyNode
+		nums = set(nums)
+
+		while cur.next:
+
+				if cur.next.val in nums:
+						cur.next = cur.next.next
+				else:
+						cur = cur.next
+		
+		return dummyNode.next
