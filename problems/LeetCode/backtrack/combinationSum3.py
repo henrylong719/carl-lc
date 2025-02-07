56c2bad991703dec9e690acd3713c236f10281f2
henrylong <henrylong719@gmail.com>
2025-02-07 18:01:48 +1100

    add leetcode Combination Sum III

--- problems/LeetCode/backtrack/combinationSum3.py
+++ problems/LeetCode/backtrack/combinationSum3.py
@@ -1,26 +1,25 @@
-class Solution:
-    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        comb = []
-        ans = []
+def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+		comb = []
+		ans = []
 
-        def backtrack(i: int, t: int) -> None:
+		def backtrack(i: int, t: int) -> None:
 
-            d = k - len(comb)
+				d = k - len(comb)
 
-            if i < d:
-                return
-            
-            if t < 0 or t > (i * 2 - d + 1) * d // 2:
-                return 
+				if i < d:
+						return
+				
+				if t < 0 or t > (i * 2 - d + 1) * d // 2:
+						return 
 
-            if len(comb) == k:
-                ans.append(comb.copy())
-                return 
+				if len(comb) == k:
+						ans.append(comb.copy())
+						return 
 
-            for j in range(i, 0, -1):
-                comb.append(j)
-                backtrack(j - 1, t - j)
-                comb.pop()
+				for j in range(i, 0, -1):
+						comb.append(j)
+						backtrack(j - 1, t - j)
+						comb.pop()
 
-        backtrack(9,n)
-        return ans
+		backtrack(9,n)
+		return ans
