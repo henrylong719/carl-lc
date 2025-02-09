fb4b19a52484c7b970913f999996914f3c1c4604
henrylong <henrylong719@gmail.com>
2025-02-09 22:23:07 +1100

    add leetcode N-Queens

---
+++ problems/LeetCode/backtrack/N-Queens.py
@@ -0,0 +1,31 @@
+def solveNQueens(self, n: int) -> List[List[str]]:
+		col = set()
+		posDiag = set() # r + c
+		negDiag = set() # r - c
+		ans = []
+
+		board = [["."] * n for col in range(n)]
+
+		def backtrack(r: int) -> None:
+				if r == n:
+						copy = ["".join(row) for row in board]
+						ans.append(copy)
+						return
+
+				
+				for c in range(n):
+						if c in col or r + c in posDiag or r - c in negDiag:
+								continue
+
+						col.add(c)
+						posDiag.add(r + c)
+						negDiag.add(r - c)
+						board[r][c] = 'Q'
+						backtrack(r + 1)
+						col.remove(c)
+						posDiag.remove(r + c)
+						negDiag.remove(r - c)
+						board[r][c] = '.'
+
+		backtrack(0)
+		return ans
