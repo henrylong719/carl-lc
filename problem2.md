58923bd3b60d8c5504c5098852f8fbdd2c303609
henrylong <henrylong719@gmail.com>
2025-02-17 12:39:05 +1100

    add Minimum Path Sum from top to bottom

--- problem2.md
+++ problem2.md
@@ -740,6 +740,23 @@ def minPathSum(self, grid: List[List[int]]) -> int:
   # Space complexity: O(r * c)
   
   
+  
+  def minPathSum(self, grid: List[List[int]]) -> int:
+    ROWS, COLS = len(grid), len(grid[0])
+
+    dp = [float("inf")] * (COLS + 1)
+    dp[COLS - 1] = 0
+
+    for row in range(ROWS - 1, -1, -1):
+        for col in range(COLS - 1, -1, -1):
+            dp[col] = min(dp[col + 1], dp[col]) + grid[row][col]
+
+    return dp[0]
+  
+  
+  # Time complexity: O(r * c)
+  # Space complexity: O(c)
+  
 ```
 
 
--- problems/LeetCode/DP/minPathSum.py
+++ problems/LeetCode/DP/minPathSum.py
@@ -12,6 +12,19 @@ def minPathSum(self, grid: List[List[int]]) -> int:
 		return ans[0][0]
 
 
+def minPathSum(self, grid: List[List[int]]) -> int:
+		ROWS, COLS = len(grid), len(grid[0])
+
+		dp = [float("inf")] * (COLS + 1)
+		dp[COLS - 1] = 0
+
+		for row in range(ROWS - 1, -1, -1):
+				for col in range(COLS - 1, -1, -1):
+						dp[col] = min(dp[col + 1], dp[col]) + grid[row][col]
+				
+		return dp[0]
+
+
 # from top to bottom
 def minPathSum(self, grid: List[List[int]]) -> int:
 
@@ -26,3 +39,6 @@ def minPathSum(self, grid: List[List[int]]) -> int:
 				return min(dfs(r - 1, c), dfs(r, c - 1)) + grid[r][c]
 
 		return dfs(len(grid) - 1, len(grid[0]) - 1)
+
+
+
