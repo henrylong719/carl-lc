7cbdc95523a90c5f54121e52aa747de951f85303
henrylong <henrylong719@gmail.com>
2025-07-29 23:05:26 +1000

    add leetcode maxProfix

---
+++ problems/LeetCode/Easy/maxProfit.ts
@@ -0,0 +1,16 @@
+function maxProfit(prices: number[]): number {
+  let lowest = prices[0];
+  let best = 0;
+
+  for (let i = 1; i <= prices.length; i++) {
+    if (prices[i - 1] < lowest) {
+      lowest = prices[i - 1];
+    }
+
+    best = Math.max(best, prices[i - 1] - lowest);
+  }
+
+  return best;
+}
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
