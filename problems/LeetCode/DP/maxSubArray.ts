c61164cf303b6a5262223f20d04958a13ac9b00a
henrylong <henrylong719@gmail.com>
2025-08-10 22:34:36 +1000

    add leetcode maxSubArray

---
+++ problems/LeetCode/DP/maxSubArray.ts
@@ -0,0 +1,13 @@
+function maxSubArray(nums: number[]): number {
+  const dp = Array(nums.length).fill(0);
+
+  dp[0] = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+  }
+
+  return Math.max(...dp);
+}
+
+console.log(maxSubArray([5, 4, -1, 7, 8]));
