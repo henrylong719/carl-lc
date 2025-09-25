569d6ee5ffc9c5d9054942db368eaaf21daece3f
henrylong <henrylong719@gmail.com>
2025-09-25 22:41:12 +1000

    add hackerRank beautifulDays

--- problems/HackerRank/Algorithms/Implementation/beautifulDays.ts
+++ problems/HackerRank/Algorithms/Implementation/beautifulDays.ts
@@ -10,3 +10,27 @@ function beautifulDays(i: number, j: number, k: number): number {
 
   return cnt;
 }
+
+function reverseNumber(num: number) {
+  let rev = 0;
+
+  while (num > 0) {
+    rev = rev * 10 + (num % 10);
+    num = Math.floor(num / 10);
+  }
+
+  return rev;
+}
+
+function beautifulDays2(i: number, j: number, k: number): number {
+  let cnt = 0;
+
+  for (let num = i; num <= j; num++) {
+    let reversed_num = reverseNumber(num);
+    if ((num - reversed_num) % k === 0) {
+      cnt++;
+    }
+  }
+
+  return cnt;
+}
