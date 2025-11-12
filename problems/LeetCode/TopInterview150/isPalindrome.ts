da2d0b548bec63a789d8c413eda2fc750db85841
henrylong <henrylong719@gmail.com>
2025-11-13 09:50:49 +1100

    add leetcode isPalindromes.ts

---
+++ problems/LeetCode/TopInterview150/isPalindrome.ts
@@ -0,0 +1,17 @@
+function isPalindrome(s: string): boolean {
+  // clear non alphabets and numbers
+  const alphaNumeric = s.replace(/[^0-9a-zA-Z]/g, '');
+
+  let l = 0;
+  let r = alphaNumeric.length - 1;
+
+  while (l < r) {
+    if (alphaNumeric[l].toLowerCase() !== alphaNumeric[r].toLowerCase()) {
+      return false;
+    }
+    l++;
+    r--;
+  }
+
+  return true;
+}
