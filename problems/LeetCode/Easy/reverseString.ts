f6fbfe0c7b9b7bdd1495b7aa6756e594ff9b62ac
henrylong <henrylong719@gmail.com>
2025-12-31 11:51:56 +1100

    add leetcode reverseString

---
+++ problems/LeetCode/Easy/reverseString.ts
@@ -0,0 +1,13 @@
+function reverseString(s: string[]): void {
+  let l = 0;
+  let r = s.length - 1;
+
+  while (l < r) {
+    [s[l], s[r]] = [s[r], s[l]];
+    l++;
+    r--;
+  }
+}
+
+// Time O(n)
+// Space O(1)
