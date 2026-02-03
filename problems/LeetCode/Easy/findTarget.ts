d6b6c3d65d5e2735978cdce91b762ef1558036d6
henrylong <henrylong719@gmail.com>
2026-02-03 15:16:03 -0600

    add leetcode findTarget

--- problems/LeetCode/Easy/findTarget.ts
+++ problems/LeetCode/Easy/findTarget.ts
@@ -18,3 +18,18 @@ function findTarget(root: TreeNode | null, k: number): boolean {
   };
   return dfs(root);
 }
+
+function findTarget(root: TreeNode | null, k: number): boolean {
+  const set = new Set();
+  const dfs = (node: TreeNode): boolean => {
+    if (!node) return false;
+    const need = k - node.val;
+    if (set.has(node.val)) return true;
+    set.add(need);
+    return dfs(node.left) || dfs(node.right);
+  };
+  return dfs(root);
+}
+
+// Time: O(n)
+// Space: O(n)
