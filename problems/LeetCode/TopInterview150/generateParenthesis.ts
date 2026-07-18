498388feffdc5c6b93ba0e4cc1525e8471b66656
Henry Long <henrylong719@gmail.com>
2026-07-18 22:21:28 +1000

    add leetcode mergeTwoLists

--- problems/LeetCode/TopInterview150/generateParenthesis.ts
+++ problems/LeetCode/TopInterview150/generateParenthesis.ts
@@ -1,27 +1,27 @@
 function generateParenthesis(n: number): string[] {
-  const ans: string[] = [];
+	const ans: string[] = [];
 
-  const backtrack = (open: number, close: number, str: string) => {
-    if (str.length === 2 * n) {
-      console.log('str', str);
+	const backtrack = (open: number, close: number, str: string) => {
+		if (str.length === 2 * n) {
+			console.log('str', str);
 
-      ans.push(str);
-      return;
-    }
+			ans.push(str);
+			return;
+		}
 
-    if (open < n) {
-      backtrack(open + 1, close, str + '(');
-    }
+		if (open < n) {
+			backtrack(open + 1, close, str + '(');
+		}
 
-    if (close < open) {
-      backtrack(open, close + 1, str + ')');
-    }
-  };
+		if (close < open) {
+			backtrack(open, close + 1, str + ')');
+		}
+	};
 
-  backtrack(0, 0, '');
-  return ans;
+	backtrack(0, 0, '');
+	return ans;
 }
 
-generateParenthesis(3);
+generateParenthesis(2);
 // Time: O(2^(2n) * n)
 // Space: O(n) recursion depth (not counting the output list)
