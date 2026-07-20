3f86c2d3bfbf2d92ed888a4dd2bf7e987407b049
Henry Long <henrylong719@gmail.com>
2026-07-20 22:56:43 +1000

    add leetcode rotate

---
+++ problems/LeetCode/TopInterview150/rotate.py
@@ -0,0 +1,40 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        n = len(nums)
+        _k = k % n
+        if _k == n:
+            return
+
+        arr = nums[(n - _k) :] + nums[: (n - _k)]
+
+        for i in range(n):
+            nums[i] = arr[i]
+            
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        n = len(nums)
+        k %= n
+
+        def reverse(left: int, right: int) -> None:
+
+            while left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+        
+        # reverse the whole array
+        reverse(0, n-1)
+        # reverse the first k elements
+        reverse(0, k-1)
+        # reverse the remaining elements
+        reverse(k, n-1)
+
+        
